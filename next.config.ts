025fd061dc2f00e703c6cc385af35e802014f93c
Leo.Liao <liaoweiqiang@dao42.com>
2025-09-14 15:29:34 +0800

    降级成next=14，移除实验性flag

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: true,
-    clientSegmentCache: true,
-    nodeMiddleware: true
-  },
   allowedDevOrigins: [
     '*.clackypaas.com',
     'localhost',
